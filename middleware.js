6a222b2d2a66019f0d9c94beda311b51619c464b
ryihuan <ryihuan@gmail.com>
2026-05-06 00:10:50 +0800

    fix passfort import

--- middleware.js
+++ middleware.js
@@ -1,5 +1,10 @@
-import { withPassfort } from '@tommyvez/passfort';
+// middleware.js - 正确版本
+import { withPasswordProtect } from '@tommyvez/passfort/next';
+
+export default withPasswordProtect({
+  protectAll: true,  // 保护全站
+});
+
 export const config = {
-    matcher: ['/((?!.*\\..*|_next).*)', '/'],
+  matcher: ['/((?!api|_next|favicon.ico).*)'],
 };
-export default withPassfort();
